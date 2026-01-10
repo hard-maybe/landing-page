8e02f62189c6c25b35e44f666288924e82c4f32a
hard-maybe <taterlaak@gmail.com>
2026-01-10 18:19:08 +1100

    updated piggyback script

--- script.js
+++ script.js
@@ -1,47 +1,4 @@
-const images = [
-  "images/bg1.jpg",
-  "images/bg2.jpg",
-  "images/bg3.jpg",
-  "images/bg4.jpg",
-  "images/bg5.jpg",
-  "images/bg6.jpg",
-  "images/bg7.jpg",
-  "images/bg8.jpg",
-  "images/bg9.jpg",
-  "images/bg10.jpg",
-  "images/bg11.jpg",
-  "images/bg12.jpg"
-];
-
-let index = 0;
-let showingFirst = true;
-
-const bg1 = document.getElementById("bg1");
-const bg2 = document.getElementById("bg2");
-
-// Set initial image
-bg1.style.backgroundImage = `url(${images[0]})`;
-bg1.style.opacity = 1;
-bg2.style.opacity = 0;
-
-setInterval(() => {
-  index = (index + 1) % images.length;
-
-  const nextBg = showingFirst ? bg2 : bg1;
-  const currentBg = showingFirst ? bg1 : bg2;
-
-  nextBg.style.backgroundImage = `url(${images[index]})`;
-  nextBg.style.opacity = 1;
-  currentBg.style.opacity = 0;
-
-updateUIColorByIndex(index);
-  
-  showingFirst = !showingFirst;
-}, 6000);
-
-
-
-// UI colours mapped to background index
+// UI colours mapped to background order (bg1 → bg12)
 const uiColors = [
   '#ffffff', // bg1
   '#6282B0', // bg2
@@ -60,12 +17,11 @@ const uiColors = [
 const root = document.documentElement;
 let index = 0;
 
-// set initial colour
-root.style.setProperty('--ui-color', uiColors[0]);
+// initial colour (matches bg1)
+root.style.setProperty('--ui-color', uiColors[index]);
 
-// advance colour every 8s (matches animation-delay)
+// advance colour every 8s (matches your CSS animation-delay)
 setInterval(() => {
   index = (index + 1) % uiColors.length;
   root.style.setProperty('--ui-color', uiColors[index]);
 }, 8000);
-
