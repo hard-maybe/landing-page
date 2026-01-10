4e5d961aa1e4f759a7073f496ec79ac5a033409f
hard-maybe <taterlaak@gmail.com>
2026-01-10 13:27:21 +1100

    added 12 background images

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const images = [
   "images/bg1.jpg",
   "images/bg2.jpg",
   "images/bg3.jpg",
-  "images/bg4.jpg"
+  "images/bg4.jpg",
+  "images/bg5.jpg",
+  "images/bg6.jpg",
+  "images/bg7.jpg",
+  "images/bg8.jpg",
+  "images/bg9.jpg",
+  "images/bg10.jpg",
+  "images/bg11.jpg",
+  "images/bg12.jpg"
 ];
 
 let index = 0;
